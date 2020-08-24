3b30b1af9689710d059cb5dbbb8aacacb4fabb5f
Jenn Youngblood <50023967+YoungbloodJenn@users.noreply.github.com>
2020-08-24 11:36:56 -0700

    Update main.js

--- LoveCalculator/main.js
+++ LoveCalculator/main.js
@@ -3,3 +3,11 @@ var partner = prompt("Enter your partner's name")
 var n = Math.random();
 n = Math.ceil(n * 100);
 alert("You are " + n + " % compatible!");
+
+if (n >= 70){
+    alert('You are mostly compatible');
+    } else if (n >50 || n <70){
+        alert('You are somewhat compatible')
+    } else {
+            alert('congrats!')
+        }
